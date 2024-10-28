711ed432334b976730ffb2077aeb5e567c5944f4
gmicroul <121873438+gmicroul@users.noreply.github.com>
2024-10-28 09:10:27 +0800

    Update hkdm.nix

--- examples/hkdm/hkdm.nix
+++ examples/hkdm/hkdm.nix
@@ -24,7 +24,7 @@
   hardware.sensor.iio.enable = true;
   #sound.enable = true;
 #  nixpkgs.config.pulseaudio = true;
-  services.libinput = true;
+  services.libinput.enable = true;
   hardware.bluetooth.enable = true;
   hardware.pulseaudio.enable = lib.mkDefault true; # mkDefault to help out users wanting pipewire
   networking.networkmanager.enable = true;
