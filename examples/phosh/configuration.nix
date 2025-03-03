4b4711c0ff8d84551676b16d40c6b52f1a83d21f
gmicroul <121873438+gmicroul@users.noreply.github.com>
2025-03-03 09:24:03 +0800

    Update configuration.nix

--- examples/phosh/configuration.nix
+++ examples/phosh/configuration.nix
@@ -25,20 +25,16 @@ in
     services.xserver.desktopManager.phosh = {
       user = defaultUserName;
     };
-    # Networking, modem and misc.
-    {
-      # Ensures any rndis config from stage-1 is not clobbered by NetworkManager
-      networking.networkmanager.unmanaged = [ "rndis0" "usb0" ];
+  
+    # Ensures any rndis config from stage-1 is not clobbered by NetworkManager
+    networking.networkmanager.unmanaged = [ "rndis0" "usb0" ];
 
-      # Setup USB gadget networking in initrd...
-      mobile.boot.stage-1.networking.enable = lib.mkDefault true;
-    }
+    # Setup USB gadget networking in initrd...
+    mobile.boot.stage-1.networking.enable = lib.mkDefault true;
+    
 
-    # SSH
-    {
-      # Start SSH by default...
-      # Not a good idea given the fact this config is insecure (well-known password).
-      services.openssh.enable = true;
-    }    
+
+    services.openssh.enable = true;
+    
   };
 }
